2533c662f5c189c15f24bef093fd4bfd9d93adc2
Michelle <michey.asmah21@gmail.com>
2021-03-06 10:17:57 -0500

    add box project

---
+++ javascript.js
@@ -0,0 +1,52 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px";
+    document.getElementById("box").style.width = "300px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor= "blue"
+    
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity= "0.4"
+    
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity= "0.4"
+    
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height= "150px"
+    document.getElementById("box").style.width= "150px"
+    document.getElementById("box").style.opacity= "1.0"
+    document.getElementById("box").style.backgroundColor= "orange"
+    document.getElementById("box").style.borderStyle= "none"
+    
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.height= "75px";
+    document.getElementById("box").style.width= "75px";
+});
+
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.borderStyle= "solid"
+    
+});
+
+document.getElementById("button7").addEventListener("click", function(){
+
+    document.getElementById("box").style.borderStyle= "dotted"
+    
+});
